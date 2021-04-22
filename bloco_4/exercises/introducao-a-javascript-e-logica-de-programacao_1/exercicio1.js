b1ce907d230168670eca5aff62fd84f1bb1a99d3
pedro21medeiros <pedro21medeiros@gmail.com>
2021-04-22 18:05:58 -0300

    Resolve exercicio 2 - maior entre 2 numeros

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -1,3 +1,5 @@
+// Exercício 1:
+
 let a = 10;
 let b = 5;
 
@@ -6,3 +8,17 @@ console.log("subração: " + (a - b));
 console.log("multiplicação: " + (a * b));
 console.log("divisão: " + (a / b));
 console.log("módulo: " + (a % b));
+
+// Exercício 2:
+
+let c = 1;
+let d = 2;
+let e = 3;
+
+switch(c > d) {
+    case "c é maior": 
+    console.log("c é maior");
+    break;
+    default: 
+    console.log("d é maior");
+}
