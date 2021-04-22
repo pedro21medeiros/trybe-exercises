ff27ddf394d973576d68f95a8e480cb1f3765004
pedro21medeiros <pedro21medeiros@gmail.com>
2021-04-22 17:55:04 -0300

    Adiciona arquivo .js com o primeiro exercicio feito

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+console.log("soma: " + (a + b));
+console.log("subração: " + (a - b));
+console.log("multiplicação: " + (a * b));
+console.log("divisão: " + (a / b));
+console.log("módulo: " + (a % b));
