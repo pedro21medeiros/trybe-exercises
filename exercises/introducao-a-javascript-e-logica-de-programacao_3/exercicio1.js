054beece7b1655b0abd6eea9e8538be831995389
pedro21medeiros <pedro21medeiros@gmail.com>
2021-04-26 18:04:14 -0300

    Resolve exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,10 @@
+let n = 3;
+let linha = '';
+
+for (let index = 0; index < n; index += 1) {
+    linha = linha + '*';
+};
+
+for (let index = 0; index < n; index += 1) {
+    console.log(linha);
+};
